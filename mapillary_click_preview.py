fb72cc865c5e97c0c00c69966b0527038751b1be
annadeckmyn <anna.deckmyn@gmail.com>
2026-03-14 07:51:41 +0100

    Enhance Mapillary Click Preview plugin: add coverage vector tiles layer and update metadata

--- mapillary_click_preview.py
+++ mapillary_click_preview.py
@@ -1,14 +1,17 @@
 
 # -*- coding: utf-8 -*-
-# QGIS Plugin: Mapillary Click Preview (toggle button)
+# QGIS Plugin: Mapillary Click Preview (toggle button + add coverage VTP layer)
 
 import os
 from qgis.PyQt.QtCore import Qt
 from qgis.PyQt.QtGui import QIcon
 from qgis.PyQt.QtWidgets import QAction, QDialog, QFormLayout, QLineEdit, QDialogButtonBox
-from qgis.core import QgsSettings
+from qgis.core import QgsSettings, QgsProject, QgsVectorTileLayer
 
 from . import mapillary_click_tool as tool
+
+
+TILES_TEMPLATE = 'https://tiles.mapillary.com/maps/vtp/mly1_public/2/{{z}}/{{x}}/{{y}}?access_token={token}'
 
 
 class TokenDialog(QDialog):
@@ -43,6 +46,7 @@ class MapillaryClickPreviewPlugin:
         self.iface = iface
         self.action = None
         self.settings_action = None
+        self.add_tiles_action = None
 
     def initGui(self):
         icon_path = os.path.join(os.path.dirname(__file__), 'icon.svg')
@@ -56,9 +60,13 @@ class MapillaryClickPreviewPlugin:
         self.settings_action = QAction('Mapillary Token…', self.iface.mainWindow())
         self.settings_action.triggered.connect(self._open_settings)
 
+        self.add_tiles_action = QAction('Add Mapillary Coverage (Vector Tiles)', self.iface.mainWindow())
+        self.add_tiles_action.triggered.connect(self._add_vector_tiles_layer)
+
         self.iface.addToolBarIcon(self.action)
         self.iface.addPluginToMenu('&Mapillary', self.action)
         self.iface.addPluginToMenu('&Mapillary', self.settings_action)
+        self.iface.addPluginToMenu('&Mapillary', self.add_tiles_action)
 
     def unload(self):
         try:
@@ -67,6 +75,8 @@ class MapillaryClickPreviewPlugin:
                 self.iface.removePluginMenu('&Mapillary', self.action)
             if self.settings_action:
                 self.iface.removePluginMenu('&Mapillary', self.settings_action)
+            if self.add_tiles_action:
+                self.iface.removePluginMenu('&Mapillary', self.add_tiles_action)
         except Exception:
             pass
 
@@ -90,3 +100,25 @@ class MapillaryClickPreviewPlugin:
         dlg = TokenDialog(self.iface.mainWindow())
         dlg.setModal(True)
         dlg.exec()
+
+    def _add_vector_tiles_layer(self):
+        s = QgsSettings()
+        token = s.value('mapillary/access_token', '', type=str).strip()
+        if not token:
+            # Prompt for token first
+            self._open_settings()
+            token = s.value('mapillary/access_token', '', type=str).strip()
+            if not token:
+                from qgis.core import QgsMessageLog, Qgis
+                QgsMessageLog.logMessage('No Mapillary token set. Cannot add vector tiles layer.', 'Mapillary', Qgis.Warning)
+                return
+
+        url = TILES_TEMPLATE.format(token=token)
+        # QGIS Vector Tile URI uses provider options style: type=xyz&url=... (see docs)
+        uri = f'type=xyz&url={url}'
+        vtl = QgsVectorTileLayer(uri, 'Mapillary Coverage (mly1_public)')
+        if not vtl.isValid():
+            from qgis.core import QgsMessageLog, Qgis
+            QgsMessageLog.logMessage('Failed to create Mapillary coverage vector tile layer. Check URL/token.', 'Mapillary', Qgis.Critical)
+            return
+        QgsProject.instance().addMapLayer(vtl)
